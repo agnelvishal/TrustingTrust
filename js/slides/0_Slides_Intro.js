647152d7e0c3e14c0f5b750480f09db5e8a41e28
agnelvishal <agnelvishal@gmail.com>
2018-03-20 17:34:15 +0530

    before pc and mobile version

--- js/slides/0_Slides_Intro.js
+++ js/slides/0_Slides_Intro.js
@@ -85,7 +85,7 @@ SLIDES.push({
 			o.loading_button.activate();
 			o.loading_button.config.onclick = function(){
 				publish("start/game");
-			screenfull.request();
+		//	screenfull.request();
 
 
 				Loader.sounds.bg_music.volume(0.75).loop(true).play(); // play music!
