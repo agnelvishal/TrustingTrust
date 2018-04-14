2936a73998b9c223b31682c3c44c829ce9b96357
agnelvishal <agnelvishal@gmail.com>
2018-04-14 16:54:31 +0530

    many

--- js/sims/Iterated.js
+++ js/sims/Iterated.js
@@ -471,10 +471,10 @@ function IteratedPeep(config){
 					// WHOOSH SOUND
 					if(config.opponent){
 						setTimeout(function(){
-							Loader.sounds.whoosh.stereo(0.9).volume(0.8).play();
+							Loader.sounds.fart.stereo(0.9).volume(0.8).play();
 						},50);
 					}else{
-						Loader.sounds.whoosh.stereo(-0.9).volume(0.8).play();
+						Loader.sounds.fart.stereo(-0.9).volume(0.8).play();
 					}
 
 					// EVIL LAUGH
